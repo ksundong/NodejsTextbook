2eeb9ab50fb4751accd2112f2b5a761405cf9391
idionkim <ksundong@gmail.com>
2019-06-22 17:44:45 +0900

    Immediately running Promise, Run All Promise
    Promise.[resolve/reject]() makes Immediately resolve or reject Promise object.
    And Promise.all run all Promises, and all Promise return resolve run then method, one Promise returns reject run catch method.

--- promise.js
+++ promise.js
@@ -25,3 +25,15 @@ promise
   .catch((error) => {
     console.log(error);   // 실패(reject)인 경우 실행
   });
+
+// Immediately running Promise(Promise.[resolve/reject]()
+const promise1 = Promise.resolve('성공1');
+const promise2 = Promise.resolve('성공2');
+// Run all Promise
+Promise.all([promise1, promise2])
+  .then((result) => {
+    console.log(result);  // ['성공1', '성공2']
+  })
+  .catch((error) => {
+    console.log(error);
+  });
